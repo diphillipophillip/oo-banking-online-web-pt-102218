f1464bd4d2221f1f2aaca63d51dc5ef29ae078f8
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-01 20:57:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
-class Transfer
-  # your code here
-end
+requrie 'pry'
+class Transfer 
+  
+  attr_reader :sender, :receiver, :number, :status, :amount
+  
+  def initialize(sender, receiver, number)
+    @sender = sender 
+    @receiver = receiver 
+    @number = number 
+    @status = "pending"
+    @amount = 50
+  end 
+  
+  def valid?
+    @sender 
+    binding.pry
+  end 
+  
+end 
